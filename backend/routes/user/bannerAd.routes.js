6d95a97349e4f2ba5e77927cae906bddb2958549
kritidigitalsolutions <info@kritidigital.com>
2026-05-08 16:11:33 +0530

    Made change.

--- backend/routes/user/bannerAd.routes.js
+++ backend/routes/user/bannerAd.routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../../controllers/bannerAd.controller");
-const {isAuth}= require("../../middlewares/auth.middleware");
+// const {isAuth}= require("../../middlewares/auth.middleware");
 
-router.get("/",isAuth, controller.getPublicBanners);
+router.get("/", controller.getPublicBanners);
 router.post("/:id/click", controller.incrementClick);
 
 module.exports = router;
